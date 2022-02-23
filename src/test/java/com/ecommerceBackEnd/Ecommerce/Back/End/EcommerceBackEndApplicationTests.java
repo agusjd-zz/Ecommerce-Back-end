88ef2312c97838dd9193f43e4fbc74b660bbcc96
Agustin Diaz <agusjd92@gmail.com>
2022-02-22 23:36:29 -0300

    first commit

---
+++ src/test/java/com/ecommerceBackEnd/Ecommerce/Back/End/EcommerceBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceBackEnd.Ecommerce.Back.End;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
